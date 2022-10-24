ae1b98ff7649aa91b1e585247b5e9c1fc5240099
changtimwu <changtimwu@ieiworld.com>
2022-10-24 18:08:38 +0800

    add vscode

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dockerfiles
 customized containers for personal use
+
+* [Visual Studio Code Server](./vscode)
+* [OpenVINO](./openvino)
---
+++ vscode/rundk.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker run -it --rm --name code-server -p 8915:8080 \
+      -v "$HOME/.config:/home/coder/.config" \
+        -v "$PWD:/home/coder/projects" \
+          -u "$(id -u):$(id -g)" \
+            -e "DOCKER_USER=$USER" \
+              codercom/code-server:latest
